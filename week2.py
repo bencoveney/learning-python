cf8fd3e5d2dfe9446b516e3fa989b5e3004913f6
Ben Coveney <bencoveney@gmail.com>
2019-02-12 13:11:02 +0000

    week 2

---
+++ week2.py
@@ -0,0 +1,10 @@
+# Variable scope
+# set of dictionaries
+
+# built_ins e.g. abs()
+# module e.g. "import math" gives sqrt()
+# function e.g. x = 5
+
+# import creates an instance of the module class
+import math
+print(dir(math))
